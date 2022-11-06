4062551db06d66c4be6df422144957a6639e6c60
Weverton Icaro <weverton.dev@gmail.com>
2022-11-06 18:12:12 -0300

    Complete Serverless + AWS course

---
+++ serverless.ts
@@ -0,0 +1,116 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'ignite-certificate',
+  frameworkVersion: '3',
+  plugins: [
+    'serverless-esbuild',
+    'serverless-dynamodb-local',
+    'serverless-offline',
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs16.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: ['dynamodb:*'],
+        Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:*'],
+        Resource: ['*'],
+      },
+    ],
+  },
+  package: {
+    individually: false,
+    patterns: ['./src/templates/**'],
+  },
+  functions: {
+    generateCertificate: {
+      handler: 'src/functions/generateCertificate.handler',
+      events: [
+        {
+          http: {
+            path: 'generateCertificate',
+            method: 'post',
+
+            cors: true,
+          },
+        },
+      ],
+    },
+    verifyCertificate: {
+      handler: 'src/functions/verifyCertificate.handler',
+      events: [
+        {
+          http: {
+            path: 'verifyCertificate/{id}',
+            method: 'get',
+
+            cors: true,
+          },
+        },
+      ],
+    },
+  },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node16',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+      external: ['puppeteer-core'],
+    },
+    dynamodb: {
+      stages: ['dev', 'local'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      dbCertificateUsers: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'users_certificate',
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: 'id',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'id',
+              KeyType: 'HASH',
+            },
+          ],
+        },
+      },
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
---
+++ src/functions/generateCertificate.ts
@@ -0,0 +1,110 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { document } from '../utils/dynamodbClient';
+import { compile } from 'handlebars';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import dayjs from 'dayjs';
+import puppeteer from 'puppeteer';
+import { S3 } from 'aws-sdk';
+
+interface ICreateCertificate {
+  id: string;
+  name: string;
+  grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), 'src', 'templates', 'certificate.hbs');
+
+  const html = readFileSync(filePath, 'utf-8');
+
+  return compile(html)(data);
+};
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
+  const response = await document
+    .query({
+      TableName: 'users_certificate',
+      KeyConditionExpression: 'id = :id',
+      ExpressionAttributeValues: {
+        ':id': id,
+      },
+    })
+    .promise();
+
+  const userAlreadyExists = response.Items[0];
+
+  if (!userAlreadyExists) {
+    await document
+      .put({
+        TableName: 'users_certificate',
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
+  }
+
+  const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png');
+
+  const medal = readFileSync(medalPath, 'base64');
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format('DD/MM/YYYY'),
+    medal,
+  };
+
+  const content = await compileTemplate(data);
+
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: 'a4',
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? './certificate.pdf' : null,
+  });
+
+  await browser.close();
+
+  const s3 = new S3();
+
+  await s3
+    .putObject({
+      Bucket: 'ignite-certificate-nodejs',
+      Key: `${id}.pdf`,
+      ACL: 'public-read',
+      Body: pdf,
+      ContentType: 'application/pdf',
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      messege: 'Certificado criado com sucesso.',
+      url: `https://ignite-certificate-nodejs.s3.sa-east-1.amazonaws.com/${id}.pdf`,
+    }),
+  };
+};
